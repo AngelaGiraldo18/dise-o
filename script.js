650ed57de5a051e437ebb993ced9d2ee18a54eeb
juanesteban22 <jestenieto@gmail.com>
2023-06-16 07:55:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById('btn-menu');
+let pNav = document.getElementById('principal-nav');
+btnMenu.addEventListener('click', function() {
+  pNav.classList.toggle('mostrar')
+})
